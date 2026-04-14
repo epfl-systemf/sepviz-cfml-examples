3af6457f9964f4d42cc475b7fbffdccb208cab84
Yawen Guan <yawen.guan@proton.me>
2026-04-14 19:51:42 +0200

    Update notations to adapt to the new sepParser

--- theories/lib/SepViz_Notations.v
+++ theories/lib/SepViz_Notations.v
@@ -1,37 +1,49 @@
 #[warnings="-notation-overridden -ambiguous-paths -notation-incompatible-prefix"]
 From CFML Require Import WPLib.
 
-Declare Custom Entry sepviz_heap.
-Notation "{*  e  *}" :=
-  (e)
-  (e custom sepviz_heap at level 200, at level 0).
+Declare Custom Entry sepviz.
 
-Notation "H1 ∗ H2" :=
-  (hstar H1 H2)
-    (in custom sepviz_heap at level 41,
-     H2 custom sepviz_heap at level 41).
-Notation "⌜  P  ⌝" :=
+Notation "⟬  e  ⟭" :=
+  (e)
+    (e custom sepviz at level 200, at level 0).
+
+Notation "'⟬' 'Pure' '┆' P '⟭'" :=
   (hpure P)
-    (in custom sepviz_heap at level 40,
-      P custom sepviz_heap at level 40).
-Notation "H1 -∗ H2" := (* star binds tighter than the wand *)
-  (hwand H1 H2)
-    (in custom sepviz_heap at level 45, right associativity).
-Notation "x ~> S" :=
+    (in custom sepviz at level 200,
+     P constr at level 200).
+
+Notation "'⟬' 'PointsTo' '┆' x '┆' S '⟭'" :=
   (repr S x)
-    (in custom sepviz_heap at level 33,
-      x custom sepviz_heap,
-      S custom sepviz_heap at level 32).
-Notation "'llet' x := a 'in' v" := ((fun x => v) a) (at level 200).
-Notation "∃  x ,  P" :=
+    (in custom sepviz at level 200,
+        x constr at level 200,
+        S constr at level 200).
+
+Notation "'⟬' 'Star' '┆' H1 '┆' H2 '⟭'" :=
+  (hstar H1 H2)
+    (in custom sepviz at level 200,
+     H1 custom sepviz at level 200,
+     H2 custom sepviz at level 200).
+
+Notation "'⟬' 'Wand' '┆' H1 '┆' H2 '⟭'" :=
+  (hwand H1 H2)
+    (in custom sepviz at level 200,
+     H1 custom sepviz at level 200,
+     H2 custom sepviz at level 200).
+
+Notation "'⟬' 'Exists' '┆' x '┆' P '⟭'" :=
   (hexists (fun x => P))
-    (in custom sepviz_heap at level 200,
-      P custom sepviz_heap at level 200).
-Notation "( x )" :=
-  (x)
-  (in custom sepviz_heap at level 0,
-    x custom sepviz_heap at level 200).
-Notation "x" :=
-  (x)
-  (in custom sepviz_heap at level 0,
-    x constr at level 200).
+    (in custom sepviz at level 200,
+      P custom sepviz at level 200).
+
+Notation "'⟬' 'Opaque' '┆' 'GC' '⟭'" :=
+  (hgc)
+    (in custom sepviz at level 200).
+
+
+Notation "'⟬' 'Opaque' '┆' 'emp' '⟭'" :=
+  (hempty)
+    (in custom sepviz at level 200).
+
+(** ** disable notations *)
+
+Notation "Q \*+ H" := (fun x => hstar (Q x) H) (only parsing): heap_scope.
